e6d78c7ff1590729d88d855e64b77e043deffd2c
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-20 15:50:48 +0200

    importation navbar bootstrap

--- index.php
+++ index.php
@@ -1,35 +1,37 @@
 <?php
 $ghibli = file_get_contents('https://ghibliapi.vercel.app/films');
 $arrayGhibli = json_decode($ghibli, true);
-var_dump($arrayGhibli);
+//var_dump($arrayGhibli);
 ?>
 <!doctype html>
 <html lang="en">
 <head>
-      <meta charset="UTF-8">
-      <meta name="viewport"
-            content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-      <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <title>Document</title>
 </head>
 <body>
 <div class="container">
-      <div class="row">
-            <?php foreach ($arrayGhibli as $movie) : ?>
-                  <div class="col-md-4 p-2">
-                        <div class="card" style="width: 18rem;">
-                              <img src="<?php echo $movie['image'] ?>" class="card-img-top" alt="...">
-                              <div class="card-body">
-                                    <h5 class="card-title"> <?php echo $movie['title'] ?></h5>
-                                    <p class="card-text"><?php echo substr($movie['description'], 0, 300) . ' ...' ?></p>
-                                    <a href="http://localhost:8080/item.php?film_id=<?php echo $movie['id'] ?>"
-                                       class="btn btn-primary">Voir plus</a>
-                              </div>
-                        </div>
+    <div class="row">
+          <?php foreach ($arrayGhibli as $movie) : ?>
+              <div class="col-md-4 p-2">
+                  <div class="card" style="width: 18rem;">
+                      <img src="<?php echo $movie['image'] ?>" class="card-img-top" alt="...">
+                      <div class="card-body">
+                          <h5 class="card-title"> <?php echo $movie['title'] ?></h5>
+                          <p class="card-text"><?php echo substr($movie['description'], 0, 300) . ' ...' ?></p>
+                            <?php foreach ($movie['people'] as $characters) ?>
+                          <a href="http://localhost:8080/item.php?film_id=<?php echo $movie['id'] ?>"
+                             class="btn btn-primary">Voir plus</a>
+                      </div>
                   </div>
-            <?php endforeach; ?>
-      </div>
+              </div>
+          <?php endforeach; ?>
+    </div>
 </div>
 </body>
 </html>
--- item.php
+++ item.php
@@ -18,22 +18,47 @@
 <?php else : ?>
 
 <?php $ghibli = file_get_contents('https://ghibliapi.vercel.app/films/' . $_GET['film_id']);
-$ghibliArray = json_decode($ghibli, true); ?>
+$ghibliArray = json_decode($ghibli, true);
+//var_dump($ghibliArray);
+?>
 
 <body class="container">
 <h1 class="text-center pt-2"> <?php echo $ghibliArray['title'] ?></h1>
-<p class="text-center"><?php echo $ghibliArray['original_title'] ?></p>
-<div class="d-flex flex-row py-4">
-    <img src="<?php echo $ghibliArray['image'] ?>">
-    <div class="container fs-5 px-5">
-        <p><?php echo $ghibliArray['description'] ?></p>
-        <p>Running time : <?php echo $ghibliArray['running_time'] ?> min</p>
-        <p>Release date : <?php echo $ghibliArray['release_date'] ?></p>
-        <p>Director : <?php echo $ghibliArray['director'] ?></p>
-        <p>Producer : <?php echo $ghibliArray['producer'] ?></p>
-        <p><?php echo $ghibliArray['rt_score'] ?>% on Rotten Tomatoes</p>
-    </div>
-</div>
+<section>
+      <p class="text-center"><?php echo $ghibliArray['original_title'] ?></p>
+      <h2>Description :</h2>
+      <div class="d-flex flex-row py-4">
+            <img src="<?php echo $ghibliArray['image'] ?>">
+            <div class="container fs-5 px-5">
+                  <p><?php echo $ghibliArray['description'] ?></p>
+                  <p>Running time : <?php echo $ghibliArray['running_time'] ?> min</p>
+                  <p>Release date : <?php echo $ghibliArray['release_date'] ?></p>
+                  <p>Director : <?php echo $ghibliArray['director'] ?></p>
+                  <p>Producer : <?php echo $ghibliArray['producer'] ?></p>
+                  <p><?php echo $ghibliArray['rt_score'] ?>% on Rotten Tomatoes</p>
+            </div>
+      </div>
+</section>
+<section>
+      <h2>Characters :</h2>
+      <ul>
+            <?php foreach ($ghibliArray['people'] as $characterLink) : ?>
+                  <?php
+                  $characterInfo = file_get_contents($characterLink);
+                  $characterArray = json_decode($characterInfo, true);
+//                  var_dump($characterArray);
+                  ?>
+                  <?php if(strlen($characterArray['gender']) > 0) : ?>
+                        <li><?php echo $characterArray['name'] ?> (<?php echo $characterArray['age']?> ans, <?php echo $characterArray['gender']?>)</li>
+                  <?php else : ?>
+                        <li><?php echo $characterArray['name'] ?> (<?php $characterArray['age']?> ans)</li>
+                  <?php endif; ?>
+            <?php endforeach; ?>
+      </ul>
+
+
+</section>
+
 
 </body>
 </html>
