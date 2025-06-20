ca0fe02f33993225bed9725488a37f2d83cab98e
alexandre-mateos <mateosalexandre95@gmail.com>
2025-06-20 14:01:22 +0200

    creation page item avec style bootstrap

--- item.php
+++ item.php
@@ -1,11 +1,3 @@
-<?php
-if(isset($_GET['film_id'])){
-      $movieGhibli = file_get_contents('https://ghibliapi.vercel.app/films/' . $_GET['film_id']);
-}else{
-      echo "Désolé ce film n'existe pas";
-}
-?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,9 +6,35 @@ if(isset($_GET['film_id'])){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
 </head>
-<body>
 
-<h1></h1>
+<?php if (!isset($_GET['film_id'])) : ?>
+
+      <?php echo "Désolé ce film n'existe pas"; ?>
+    <a href="index.php" class="btn btn-primary">Retour à l'accueil</a>
+
+<?php else : ?>
+
+<?php $ghibli = file_get_contents('https://ghibliapi.vercel.app/films/' . $_GET['film_id']);
+$ghibliArray = json_decode($ghibli, true); ?>
+
+<body class="container">
+<h1 class="text-center pt-2"> <?php echo $ghibliArray['title'] ?></h1>
+<p class="text-center"><?php echo $ghibliArray['original_title'] ?></p>
+<div class="d-flex flex-row py-4">
+    <img src="<?php echo $ghibliArray['image'] ?>">
+    <div class="container fs-5 px-5">
+        <p><?php echo $ghibliArray['description'] ?></p>
+        <p>Running time : <?php echo $ghibliArray['running_time'] ?> min</p>
+        <p>Release date : <?php echo $ghibliArray['release_date'] ?></p>
+        <p>Director : <?php echo $ghibliArray['director'] ?></p>
+        <p>Producer : <?php echo $ghibliArray['producer'] ?></p>
+        <p><?php echo $ghibliArray['rt_score'] ?>% on Rotten Tomatoes</p>
+    </div>
+</div>
+
 </body>
 </html>
+<?php endif; ?>
